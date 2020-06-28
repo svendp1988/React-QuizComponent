29ad3c3c8528859030c91bdb74b8a38bca8d6a5c
Sven De Potter <sven.depotter@hotmail.com>
2020-06-28 20:58:15 +0200

    module1

---
+++ src/Quiz.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+let quizData = require("./quiz_data.json");
+
+class Quiz extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            quiz_position: 1,
+        }
+    }
+
+    render() {
+        console.log(quizData);
+        return (
+            <div>
+                <div
+                    className="QuizQuestion"
+                >
+                    {quizData.quiz_questions[0].instruction_text}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Quiz;
